fa16c23578769bea93296f28f197bcc714938a46
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-10-28 23:52:55 +0530

    sub16

--- ssmp/sub16.asm
+++ ssmp/sub16.asm
@@ -12,6 +12,7 @@ START:
     XOR CX,CX   ;Clear the Borrow Register
     MOV AX,N1   ;Move First Number to AX
     MOV BX,N2   ;Move Second Number to BX
+    
     SUB AX,BX   ;Subtract and store value in AX
     JNC STOP    ;if to STOP(final part of program) if no carry/borrow (JNC-Jump No Carry)
     INC CX      ;if carry/borrow, increment CX  
